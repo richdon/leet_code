6f6db31e8f792d5e3b5f9d9bf1d3b231a3cf0e28
Richard Donald <richard_donald92@outlook.com>
2024-11-14 17:19:28 -0500

    kth_largest

--- kthLargest.py
+++ kthLargest.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+class KthLargest:
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    def __init__(self, k: int, nums: list[int]):
+        self.k = k
+        self.nums = [self.add(n) for n in nums]
+
+    def get_parent_idx(self, i):
+        return i // 2
+
+    def get_right_child_idx(self, i):
+        return 2 * i + 1
+
+    def get_left_child_idx(self, i):
+        return 2 * i + 2
+
+    def should_swap(self, idx):
+        if idx == 0:
+            return False
+        parent_idx = self.get_parent_idx(idx)
+        return self.nums[idx] < self.nums[parent_idx]
+
+    def swap(self, idx, element):
+        parent_idx = self.get_parent_idx(idx)
+        self.nums[idx] = self.nums[parent_idx]
+        self.nums[parent_idx] = element
+        return parent_idx
+
+    def heapify_up(self):
+        idx = len(self.nums) - 1
+        while self.should_swap(idx):
+            element = self.nums[idx]
+            idx = self.swap(idx, element)
+
+    def kthLargest(self, k, nums):
+        self.k = k
+        self.nums = nums
+        self.heapify_up()
+        return self.check_kth_largest()
+
+    def check_kth_largest(self):
+        j = len(self.nums)
+        idx = 0
+        while j > idx and j != self.k:
+            left_child = self.nums[self.get_left_child_idx(idx)]
+            right_child = self.nums[self.get_right_child_idx(idx)]
+            val = self.nums[idx]
+            if left_child > val or right_child >
+
+    def add(self, val: int) -> int:
+        self.nums.append(val)
+        self.heapify_up()
+        return self.check_kth_largest()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+if __name__ == "__main__":
+    ...
